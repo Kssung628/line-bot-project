ffc7706c4c6f9fbdf430243c0966eea013caca6b
Sung Yunhao <37853776+Kssung628@users.noreply.github.com>
2025-11-24 09:16:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -232,6 +232,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Insurance LINE Bot running on port", PORT);
+   app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
 });
